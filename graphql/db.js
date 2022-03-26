35506eece0a70a0c86fc5cbe820e77ca7cf59e16
hy <mintlib66@gmail.com>
2022-03-26 16:52:00 +0900

    delete unnecessary comment

--- graphql/db.js
+++ graphql/db.js
@@ -13,33 +13,3 @@ export const getMovies = (limit, rating) => {
             .then(res => res.json())
             .then(json => json.data.movies);
 };
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-};
-
-/*
-export const deleteMovie = (id) => {
-    //현재 id 외의 movie 
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-
-    //id 존재하면 movies에서 해당 movie 삭제
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    }else{
-        return false; 
-    }
-};
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-};
-*/
